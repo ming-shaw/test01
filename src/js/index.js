9eae4e67d25da6a27f4e348eb30520d94a578962
xm_1898 <xm_1898@163.com>
2018-03-12 00:16:08 +0800

    设置nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 /**
  * Created by SHAW on 2018/3/11.
  */
+;(function () {
+    var app = angular.module("app",["ui.router"]);
+    app.controller("myController",["$scope",function ($scope) {
+    }])
+    app.directive("navs",function () {
+        return{
+            restrict:"EAMC",
+            templateUrl:"../views/nav_tpl.html",
+            controller:["$scope",function ($scope) {
+            }]
+        }
+    })
+    app.directive("tabbar",function () {
+        return{
+            restrict:"EA",
+            templateUrl:"../views/tabbar_tpl.html",
+            controller:["$scope",function ($scope) {
+
+            }]
+        }
+    })
+})(angular)
